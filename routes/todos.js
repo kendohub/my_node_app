543e7423190beea8b114f686145c9c0dd21d32db
k-endo <k-endo@ttc-net.co.jp>
2022-05-12 23:11:10 +0000

    Add creating todo

--- routes/todos.js
+++ routes/todos.js
@@ -3,31 +3,50 @@ var router = express.Router();
 
 /* GET todos listing. */
 router.get('/', function(req, res, next) {
-  const mysql = require('mysql')
+  const mysql = require('mysql');
   const connection = mysql.createConnection({
     host: 'localhost',
     user: 'my_node_app',
     password: 'my_node_app',
     database: 'my_node_app'
-  })
+  });
   
-  connection.connect()
+  connection.connect();
   
   connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    if (err) throw err
+    if (err) throw err;
   
-    console.log()
     res.send(`The solution is: ${rows[0].solution}`);
   })
   
-  connection.end()
+  connection.end();
 });
 
 /* POST create todo. */
 router.post('/', function(req, res, next) {
   const title = req.body.title;
   const content = req.body.content;
-  res.send(`respond with a new resource ${title} ${content}`);
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'my_node_app',
+    password: 'my_node_app',
+    database: 'my_node_app'
+  });
+
+  connection.connect();
+
+  const query = 'INSERT INTO `todos` (`title`, `content`, `created_at`, `updated_at`)' +
+    'VALUES (?, ?, SYSDATE(), SYSDATE())';
+  connection.query(query,
+    [title, content],
+    (err, rows, fields) => {
+      if (err) throw err;
+
+      res.send(`respond with a new resource ${title} ${content}`);
+  });
+
+  connection.end();
 });
 
 /* GET show todo. */
