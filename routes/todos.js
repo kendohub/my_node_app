e04d948f165ce7fb7a64c4e7737d43d81d9d672b
k-endo <k-endo@ttc-net.co.jp>
2022-05-23 22:04:49 +0000

    Add searching todos

--- routes/todos.js
+++ routes/todos.js
@@ -17,11 +17,17 @@ router.use((req, res, next) => {
 router.get('/', function(req, res, next) {
   const connection = getDbConnection();
   connection.connect();
+
+  const fromDate = new Date(req.query.fromDate);
+  const toDate = new Date(req.query.toDate);
   
-  connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  const query = 'SELECT * FROM `todos` WHERE ? <= `created_at` AND `updated_at` < ?';
+  const q = connection.query(query,
+    [fromDate, toDate],
+    (err, rows, fields) => {
     if (err) throw err;
   
-    res.send(`The solution is: ${rows[0].solution}`);
+    res.render('todos/index', { todos: rows });
   })
   
   connection.end();
